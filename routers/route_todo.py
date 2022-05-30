cda4d39c64efd4a4576eed2931e2b13fd400f66a
fukuba-shuma <calculus.fukuba@gmail.com>
2022-05-30 23:12:33 +0900

    delete and update method

--- routers/route_todo.py
+++ routers/route_todo.py
@@ -1,13 +1,11 @@
-from operator import rshift
-import re
+from doctest import DocTestFailure
 from typing import List
 from http.client import HTTPException
-from urllib import response
 from fastapi import APIRouter
 from fastapi import Response, Request
 from fastapi.encoders import jsonable_encoder
-from schemas import Todo, TodoBody
-from database import db_create_todo, db_get_todos
+from schemas import Todo, TodoBody, SuccessMsg
+from database import db_create_todo, db_get_todos, db_update_todo, db_delete_todo
 from starlette.status import HTTP_201_CREATED
 
 router = APIRouter()
@@ -38,4 +36,25 @@ async def get_single_todo(id: str):
         return res
     raise HTTPException(
         status_code=404, detail=f"task of Id: {id} doesn't exist"
+    )
+
+
+@router.put("/pi/todo/{id}", response_model=Todo)
+async def update_todo(id: str, data: TodoBody):
+    todo = jsonable_encoder(data)
+    res = await db_update_todo(id, todo)
+    if res:
+        return res
+    raise HTTPException(
+        status_code=404, detail="Updatte task failed"
+    )
+
+
+@router.delete("/api/todo/{id}", response_model=SuccessMsg)
+async def delete_todo(id: str):
+    res = await db_delete_todo(id)
+    if res:
+        return {"message": "Successfully deleted"}
+    raise HTTPException(
+        status_code=404, detail="delete task failed"
     )
